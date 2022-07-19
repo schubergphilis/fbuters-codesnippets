f4d460ee6da303d0f572b7ae3d98c9d4fc75a7e5
Frank Buters <fbuters@schubergphilis.com>
2022-07-19 12:43:19 +0200

    Script to generate json message from json schema file

---
+++ volksbank_transaction_datastore/generate_json_message.py
@@ -0,0 +1,44 @@
+# %%
+import json
+from jsf import JSF
+import random
+import os
+
+
+def generate_save_fake_json(faker: JSF, contract_id: int, output_filename: str):
+    """
+    Args:
+        faker (JSF): fake json generator object
+        contract_id (int): contract id to be added to the json message
+        output_filename (str): file name of output json file
+    """
+
+    test_message = faker.generate()
+    # Make sure we have have groups of the same contract number
+    test_message["boeking_referentie"]["eb_sleutel"]["contractnummer"] = contract_id
+
+    with open(f"{output_filename}.json", "w") as json_file:
+        json.dump(test_message, json_file)
+
+
+# Generate 100 contract ids
+N_contracts = 2
+N_messages_per_contract = 5
+contract_ids = random.sample(range(10000000, 99999999), N_contracts)
+
+# File path to save messages
+os.chdir("Test_JSON_Messages")
+
+# I obtained the schema by using the supplied example message and this website:
+# https://www.jsonschema.net/
+f = open("example_transaction.schema.json")
+schema = json.load(f)
+
+# Initialise fake JSON generator from json schema
+faker = JSF.from_json("example_transaction.schema.json")
+
+for contract_id in contract_ids:
+    for message_number in range(0, N_messages_per_contract):
+        output_filename = f"contract_id_{contract_id}_message_id{message_number}"
+        generate_save_fake_json(faker, contract_id, output_filename)
+# %%
